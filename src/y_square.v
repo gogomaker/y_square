38142cc69b4db3ff96f987fac66943a022fa8677
Hayong, Son <84710995+gogomaker@users.noreply.github.com>
2026-02-20 19:42:32 +0900

    Update y_square.v for test

--- src/y_square.v
+++ src/y_square.v
@@ -18,9 +18,9 @@ module tt_um_ysquare (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = 0;
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  assign uo_out  = uio_oe ? ui_in : uio_in;
+  assign uio_out = uio_oe ? ui_in : 8'bz;
+  assign uio_oe  = &ui_in;
 
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0};
