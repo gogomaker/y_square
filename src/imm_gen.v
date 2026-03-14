2213091b837dd4ff4a292d7da65c24efcfbf1015
Hayong, Son <84710995+gogomaker@users.noreply.github.com>
2026-03-14 15:44:11 +0900

    Modify imm_extended assignment for instruction decoding
    imm을 받지 않고 IR을 통채로 받기 때문에, 해당 구문을 수정함.

--- src/imm_gen.v
+++ src/imm_gen.v
@@ -7,5 +7,5 @@ module imm_gen(
   input wire [2:0] PC
 );
   assign imm_address_extended = {PC, ir[12:0]};  //word address를 상정하고 만든 것임.
-  assign imm_extended = (ir[15:13] == 3'b010) ? {10'b0, imm} : {10{imm[5]}, imm};
+  assign imm_extended = (ir[15:13] == 3'b010) ? {9'b0, ir[6:0]} : {9{ir[6]}, ir[6:0]};
 endmodule
