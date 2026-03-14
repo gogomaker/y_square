76131986197d37e2fe70752be92834f920c0d7c4
Hayong, Son <84710995+gogomaker@users.noreply.github.com>
2026-03-14 12:13:07 +0900

    Update imm_gen.v
    바뀐 ISA정의에 맞게끔 수정함.

--- src/imm_gen.v
+++ src/imm_gen.v
@@ -3,11 +3,9 @@
 module imm_gen(
   output wire [15:0] imm_address_extended,
   output wire [15:0] imm_extended,
-  input wire [11:0] imm_address,
-  input wire [5:0] imm,
-  input wire [3:0] OPcode,
+  input wire [15:0] ir,
   input wire [2:0] PC
 );
-  assign imm_address_extended = {PC, imm_address, 0};  //byte address를 상정하고 만든 것임.
-  assign imm_extended = (OPcode == 4'b1010) ? {10'b0, imm} : {10{imm[5]}, imm};
+  assign imm_address_extended = {PC, ir[12:0]};  //word address를 상정하고 만든 것임.
+  assign imm_extended = (ir[15:13] == 3'b010) ? {10'b0, imm} : {10{imm[5]}, imm};
 endmodule
