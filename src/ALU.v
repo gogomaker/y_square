3972a5eb46115e2a80fd4b7905ea44fbec54a98a
Hayong, Son <84710995+gogomaker@users.noreply.github.com>
2026-03-14 13:35:21 +0900

    ALU에 시프트 기능 통합
    ALU와 시프터를 연결할 포트를 제작함. 나머지 제어신호는 controller에서 주어야 함.

--- src/ALU.v
+++ src/ALU.v
@@ -2,19 +2,23 @@
 
 module ALU(
   output wire zero,          //for controller
-  output wire done_shift,    //for controller
+  output wire [3:0] shamt,   //for shifter
+  output wire direction      //for shifter
   output reg [15:0] answer,  //for ALU
   input wire [15:0] A,       //for ALU
   input wire [15:0] B,       //for ALU
   input wire [2:0] mode      //for ALU
   input wire [15:0] shift_answer //for shifter
 );
+
     // 1. 가산기 통합 (Add/Sub/SLT를 하나로)
   wire [15:0] sub_B = (mode == 3'd1 || mode == 3'd5) ? ~B : B;
   wire cin = (mode == 3'd1 || mode == 3'd5) ? 1'b1 : 1'b0;
   wire [15:0] sum_result = A + sub_B + cin;
 
   // 2. 시프터 연결
+  assign shamt = B[3:0];
+  assign direction = mode[0];
 
   // 3. 결과 선택 (Mux 구조)
   always @(*) begin
