b6d8443cbfbfd698cf4a072c3e9cda07c3a959b0
Hayong, Son <84710995+gogomaker@users.noreply.github.com>
2026-03-28 14:34:39 +0900

    Refactor CPU module to include new signals
    개선된 모듈 버전을 기록하기 위해 수정함.

--- src/CPU.v
+++ src/CPU.v
@@ -6,6 +6,7 @@ module CPU(
   output wire [15:0] parallel_out_shifter,
   output wire [3:0] shamt,
   output wire direction,
+  output wire sel_sr,
   input wire [15:0] data_memory_in,
   input wire clk,
   input wire reset_n
@@ -17,13 +18,17 @@ module CPU(
   cpu_controller controller(
     .zero_flag(zero_flag),
     .OPcode(OPcode),
-    .sel_address(sel_address),
-    .sel_PCconst(sel_PCconst),
-    .EN_pc(EN_pc),
-    .EN_ir(EN_ir),
-    .EN_rf(EN_rf),
-    .sel_A(sel_A),
-    .sel_B(sel_B),
+    .sel_address(sel_address),  // from controller
+    .sel_PCconst(sel_PCconst),  // from controller
+    .sel_Rs2(sel_Rs2),
+    .sel_Rd(sel_Rd),
+    .sel_write(sel_write),
+    .sel_A(sel_A),  // from controller
+    .sel_B(sel_B),  // from controller
+    .sel_sr(sel_sr),
+    .EN_pc(EN_pc),  // from controller
+    .EN_ir(EN_ir),  // from controller
+    .EN_rf(EN_rf),  // from controller
     .clk(clk),
     .reset_n(reset_n)
   );
@@ -36,16 +41,19 @@ module CPU(
     .shamt(shamt), // to shifter
     .direction(direction),// to shifter
     .zero_flag(zero_flag),  // to controller
-    .OPcode(OPcode),  // to controller
+    .OPcode_ctr(OPcode),  // to controller
 
     .parallel_in_shifter(), // from shifter
     .sel_address(sel_address),  // from controller
     .sel_PCconst(sel_PCconst),  // from controller
+    .sel_Rs2(sel_Rs2),
+    .sel_Rd(sel_Rd),
+    .sel_write(sel_write),
+    .sel_A(sel_A),  // from controller
+    .sel_B(sel_B),  // from controller
     .EN_pc(EN_pc),  // from controller
     .EN_ir(EN_ir),  // from controller
     .EN_rf(EN_rf),  // from controller
-    .sel_A(sel_A),  // from controller
-    .sel_B(sel_B),  // from controller
     .clk(clk),
     .reset_n(reset_n)
 );
