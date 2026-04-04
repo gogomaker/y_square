de90eb38cdf2a057cc96c1d0a137137c72b4b9d4
Hayong, Son <84710995+gogomaker@users.noreply.github.com>
2026-04-04 16:08:21 +0900

    Refactor CPU and SPI module connections in y_square.v
    완성한 SPI모듈과 수정한 시프터 모듈, 수정한 CPU 모듈을 모두 적절하게 연결하였다. 이제 남은 일은, 외부 포트와 연결하는 일이다.

--- src/y_square.v
+++ src/y_square.v
@@ -17,37 +17,59 @@ module tt_um_ysquare (
   input  wire       rst_n     // reset_n - low to reset
 );
 
+  wire start_read_mem, start_write_mem, read_done, write_done, sel_sr;
+  wire direction_of_CPU, shifting_CPU, shifting_SPI, sr_parallel_loading;
+  wire [15:0] address, data_for_spi, sr_parallel_out, parallel_out_shifter;
   CPU cpu(
-    .address_memory(),
-    .data_memory_in(),
-    .data_memory_out(),
-    .parallel_out_shifter(),
-    .shamt(),
-    .direction(),
-    .sel_sr(),
+    .address_memory(address),
+    .data_memory_in(sr_parallel_out),
+    .data_memory_out(data_for_spi),
+    .parallel_out_shifter(parallel_out_shifter),
+    .start_shifting(shifting_CPU),
+    .direction(direction_of_CPU),
+    .sr_parallel_loading(sr_parallel_loading),
+    .sel_sr(sel_sr),
     //memory control signal port
-    .start_read_mem(),
-    .start_write_mem(),
-    .read_done(),
-    .write_done(),
+    .start_read_mem(start_read_mem),
+    .start_write_mem(start_write_mem),
+    .read_done(read_done),
+    .write_done(write_done),
     //system port
     .clk(clk),
     .reset_n(rst_n)
   );
 
-  shifter s(
-    .answer(),
-    .done(),
-    .shamt(),
-    .parallel_in(),
-    .serial_in(),
-    .start(),
-    .direction(),  //0이 right, 1이 left
+  module SPI(
+    // CPU 컨트롤러와의 인터페이스
+    .address(address),
+    .data(data_for_spi),
+    .start_read_mem(start_read_mem),
+    .start_write_mem(start_write_mem),
+    .read_done(read_done),
+    .write_done(write_done),
+    .start_shifting(shifting_SPI),
+    // 외부 SPI 물리 핀 (메모리와 연결)
+    .CS_n(uio_out[2]),  // Active LOW Chip Select
+    .SCLK(uio_out[3]),  // SPI Clock
+    .MOSI(uio_out[4]),  // Master Out Slave In
+    .MISO(uio_in[5]),   // Master In Slave Out (외부 시프터가 직접 받을 수도 있음)
+    // 시스템 신호
     .clk(clk),
-    .reset_n(rst_n)
+    .reset_n(reset_n)
   );
-  
+
+  shifter s(
+    .answer(sr_parallel_out),
+    .parallel_in(parallel_out_shifter),
+    .serial_in(uio_in[5]),
+    .en_shift(sel_sr ? shifting_CPU : shifting_SPI),
+    .en_load(sel_sr ? sr_parallel_loading : 1'b0),     // 병렬 로딩 활성화 신호
+    .direction(sel_sr ? direction_of_CPU : 1'b1),   // 1: Left, 0: Right
+    .clk(clk),
+    .reset_n(reset_n)
+  );
+
   wire unused;
   assign unused = ena;
-
+  assign uio_oe = 8'b11011110;
 endmodule
