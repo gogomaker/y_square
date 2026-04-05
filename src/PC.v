a1ffb9c47e6855846df5b9def30be6ba733fed70
Hayong, Son <84710995+gogomaker@users.noreply.github.com>
2026-04-05 13:15:00 +0900

    Refactor address logic with LOAD input signal

--- src/PC.v
+++ src/PC.v
@@ -5,17 +5,18 @@ module PC(
   input wire [15:0] load_constant,
   input wire clk,
   input wire reset_n, 
-  input wire EN
+  input wire EN,
+  input wire LOAD
 );
 
-  always @(posedge clk, negedge reset_n) begin
+  always @(posedge clk or negedge reset_n) begin
     if(!reset_n)
       address <= 16'd32;
-    else begin
-      if(EN)
-        address <= address + 16'd1;
-      else 
+    else if (EN) begin
+      if (LOAD)
         address <= load_constant;
+      else
+        address <= address + 16'd1;
     end
   end
 endmodule
