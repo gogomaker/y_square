cead1d1d9d8d3f5999a938bb1d9067471e8a268b
Hayong, Son <84710995+gogomaker@users.noreply.github.com>
2026-04-11 15:39:26 +0900

    Update PC.v

--- src/PC.v
+++ src/PC.v
@@ -5,18 +5,14 @@ module PC(
   input wire [15:0] load_constant,
   input wire clk,
   input wire reset_n, 
-  input wire EN,
-  input wire LOAD
+  input wire EN
 );
 
   always @(posedge clk or negedge reset_n) begin
     if(!reset_n)
       address <= 16'd32;
     else if (EN) begin
-      if (LOAD)
-        address <= load_constant;
-      else
-        address <= address + 16'd1;
+      address <= load_constant;
     end
   end
 endmodule
