8804d567e201dafa1b4f2ac5adcd5c92f6fdd5a6
Hayong, Son <84710995+gogomaker@users.noreply.github.com>
2026-04-19 16:15:31 +0900

    Update address_decoder.v

--- src/address_decoder.v
+++ src/address_decoder.v
@@ -30,22 +30,22 @@ module address_decoder(
     end
   end
   wire mem_access = while_mem_access | start_read | start_write;
-  // 1. 하위 I/O 영역 판단 (0x0000 ~ 0x001F)
+  // 1. 하위 I/O 영역 판단 (0x0000 ~ 0x000F)
   // 상위 11비트(15~5)가 모두 0이면 I/O 영역입니다.
-  wire is_mem_range = |address[15:5];
+  wire is_mem_range = |address[15:4];
 
   // 2. 장치별 상세 디코딩 (비트 4와 3을 이용한 MUX 구조)
   // 00xxx (0x00~0x07): Digital In
-  assign din_en     = mem_access & !is_mem_range & (address[4:3] == 2'b00);
+  assign din_en     = mem_access & !is_mem_range & (address[1:0] == 2'b00);
   
   // 01xxx (0x08~0x0F): Digital Out
-  assign dout_en    = mem_access & !is_mem_range & (address[4:3] == 2'b01);
+  assign dout_en    = mem_access & !is_mem_range & (address[1:0] == 2'b01);
   
   // 10xxx (0x10~0x17): UART RX
-  assign uart_rx_en = mem_access & !is_mem_range & (address[4:3] == 2'b10);
+  assign uart_rx_en = mem_access & !is_mem_range & (address[1:0] == 2'b10);
   
   // 11xxx (0x18~0x1F): UART TX
-  assign uart_tx_en = mem_access & !is_mem_range & (address[4:3] == 2'b11);
+  assign uart_tx_en = mem_access & !is_mem_range & (address[1:0] == 2'b11);
 
   // 3. SPI FRAM 영역 (0x0020 이상 모든 주소)
   // I/O 영역이 아니면 모두 메모리 접근으로 간주합니다.
